9f6aba4846bd9426cc534e67f9adc638d0333b20
Yu Sun <usunyu@gmail.com>
2018-12-07 20:20:57 -0800

    Fix fetch latest bug

--- tiktok_crawler/main.py
+++ tiktok_crawler/main.py
@@ -66,7 +66,6 @@ class TikTokCrawler(object):
     def _fetch_user_media(self, user_id, dytk, url):
 
         if not user_id:
-            print("Number %s does not exist" % user_id)
             return { 'count': 0, 'list': [] }
 
         hostname = urllib.parse.urlparse(url).hostname
@@ -88,13 +87,11 @@ class TikTokCrawler(object):
             if max_cursor:
                 user_video_params['max_cursor'] = str(max_cursor)
 
-            print("Fetching user videos page %s" % page)
-
             res = requests.get(user_video_url, headers=HEADERS, params=user_video_params)
             try:
                 contentJson = json.loads(res.content.decode('utf-8'))
             except:
-                print("Fetch exception, try again!")
+                # fetch exception, try again
                 continue
 
             aweme_list = contentJson.get('aweme_list', [])
@@ -131,11 +128,8 @@ class TikTokCrawler(object):
 
             time.sleep(SLEEP_TIME)
 
-            if self.type == FETCH_LATEST:
+            if self.type == FETCH_LATEST and len(aweme_list) > 0:
                 break
-
-        if videos['count'] == 0:
-            print("There's no video in number %s." % user_id)
 
         return videos
 
@@ -147,12 +141,10 @@ class TikTokCrawler(object):
         user_id = number[0]
         videos = self._fetch_user_media(user_id, dytk, self.url)
         sys.stdout.write(str(videos).replace("'", '"'))
-        print("\n\nTik Tok user %s, video count %d\n\n" % (user_id, videos['count']))
 
     def _fetch_challenge_media(self, challenge_id, url):
 
         if not challenge_id:
-            print("Challenge #%s does not exist" % challenge_id)
             return { 'count': 0, 'list': [] }
 
         hostname = urllib.parse.urlparse(url).hostname
@@ -176,13 +168,11 @@ class TikTokCrawler(object):
                 challenge_video_params['cursor'] = str(cursor)
                 challenge_video_params['_signature'] = self.generateSignature(str(challenge_id) + '9' + str(cursor))
 
-            print("Fetching challenge videos page %s" % page)
-
             res = requests.get(challenge_video_url, headers=HEADERS,params=challenge_video_params)
             try:
                 contentJson = json.loads(res.content.decode('utf-8'))
             except:
-                print("Fetch exception, try again!")
+                # fetch exception, try again
                 continue
 
             aweme_list = contentJson.get('aweme_list', [])
@@ -220,9 +210,6 @@ class TikTokCrawler(object):
             if self.type == FETCH_LATEST:
                 break
 
-        if videos['count'] == 0:
-            print("There's no video in challenge %s." % challenge_id)
-
         return videos
 
     def fetch_challenge_videos(self):
@@ -231,7 +218,6 @@ class TikTokCrawler(object):
         challenges_id = challenge[0]
         videos = self._fetch_challenge_media(challenges_id, self.url)
         sys.stdout.write(str(videos).replace("'", '"'))
-        print("\n\nTik Tok challenge #%s, video count %d\n\n" % (challenges_id, videos['count']))
 
     def fetch(self):
         if re.search('share/user', self.url):
